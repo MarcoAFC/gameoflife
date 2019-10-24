802f34930db20196b54c5dd4c182abf594260dbc
Nammur <32174801+Nammur@users.noreply.github.com>
2019-10-24 17:09:06 -0300

    Add files via upload

--- include/life.h
+++ include/life.h
@@ -11,7 +11,7 @@ class Life{
     public:
         std::string hash;
         void set_alive(std::vector<std::vector<int>> &mat, int nlinhas, int ncol,std::multimap <int, int> &map);
-        void contar_vizinho(std::vector<std::vector<int>> &mat,std::vector<std::vector<int>> &mat2,std::multimap <int, int> &map, int nasce, int morre);
+        void contar_vizinho(std::vector<std::vector<int>> &mat,std::vector<std::vector<int>> &mat2,std::multimap <int, int> &map);
         void guardar_vizinhos(std::vector<std::vector<int>> &mat,std::vector<std::vector<int>> &mat2, std::multimap <int, int> &map,std::multimap <int, int> &map2);
         int pararExec(std::vector<std::string> &chave, int ligado);
 };
--- include/matriz.h
+++ include/matriz.h
@@ -7,4 +7,4 @@
 void copiarMat(std::vector<std::vector<int>> &mat, std::vector<std::vector<int>> &mat2, int nlinhas, int ncol);
 void iniciarBorda(int nlinhas, int ncol, std::vector<std::vector<int>> &mat);
 void printMat(int nlinhas, int ncol,std::vector<std::vector<int>> &mat);
-void printMat(int nlinhas, int ncol,std::vector<std::vector<int>> &mat, std::ofstream &myfile);
+void printMat(int nlinhas, int ncol,std::vector<std::vector<int>> &mat, std::ofstream &myfile, int geracao);
